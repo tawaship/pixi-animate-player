632d7fb59820560433d60791fb8bb83ca27aff02
iiham <iihama@synk.co.jp>
2020-08-24 12:36:58 +0900

    auto

--- rollup.config.js
+++ rollup.config.js
@@ -35,7 +35,7 @@ const pixim_banner = [
 
 export default (async () => {
 	if (process.env.PROD) {
-		await del(['./docs/pixi/', './docs/pixim/', './dist/']);
+		await del(['./pages/docs/pixi/', './pages/docs/pixim/', './dist/']);
 	}
 	
 	return [
